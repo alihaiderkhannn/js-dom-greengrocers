1762dfdbfb55b22abb1cf78a643a62db61ab832f
Ali Haider Khan <ali_786@live.no>
2024-09-24 20:52:57 +0200

    working

--- index.js
+++ index.js
@@ -53,3 +53,127 @@ const state = {
   ],
   cart: []
 };
+
+function render() {}
+render();
+
+const storeList = document.querySelector(".store--item-list");
+const cartList = document.querySelector(".cart--item-list");
+
+function handleItemClick(storeItem) {
+  
+  let c = state.cart.find(x => x.id === storeItem.id)
+
+  if (c){
+    c.quantity++
+  }
+  else {
+    let cartitem = {
+      id: storeItem.id,
+      name: storeItem.name,
+      price: storeItem.price,
+      quantity: 1
+  
+    }
+    state.cart.push(cartitem)
+  }
+
+  renderCart();
+}
+function renderItems() {
+  state.items.map((item) => {
+    let li = renderItem(item);
+    storeList.appendChild(li);
+  });
+}
+function renderCart() {
+  //iterate through state.cart.map  and update the cart view
+  state.cart.map((item) => {
+    let li = renderCartItem(item);
+    cartList.appendChild(li);
+  });
+}
+function renderCartItem(cartItem) {
+  /*
+  <li>
+  <img
+    class="cart--item-icon"
+    src="assets/icons/001-beetroot.svg"
+    alt="beetroot"
+  />
+  <p>beetroot</p>
+  <button class="quantity-btn remove-btn center">-</button>
+  <span class="quantity-text center">1</span>
+  <button class="quantity-btn add-btn center">+</button>
+</li>
+
+  */
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+
+
+  const list = document.querySelector(".cart--item-list");
+  list.innerHTML = "";
+
+  state.cart.forEach(x => {
+    const item = document.createElement("li")
+    item.innerHTML = `<img
+    class="cart--item-icon"
+    src="assets/icons/${x.id}.svg"
+    alt="${x.name}"
+  />
+  <p>${x.name}</p>
+  <button class="quantity-btn remove-btn center">-</button>
+  <span class="quantity-text center">${x.quantity}</span>
+  <button class="quantity-btn add-btn center">+</button>` 
+
+  const removebtn = item.querySelector(".remove-btn")
+  const addbtn = item.querySelector(".add-btn")
+  
+  removebtn.addEventListener('click', () => {
+    if (x.quantity > 1) {
+      x.quantity--;
+    } else {
+      state.cart = state.cart.filter(cartItem => cartItem.id !== x.id)
+    }
+    renderCart();
+  })
+
+  addbtn.addEventListener('click', () => {
+    x.quantity++;
+
+    renderCart();
+  } )
+
+  
+  list.appendChild(item)
+  });
+
+  
+ 
+}
+function renderItem(storeItem) {
+  console.log(storeItem);
+  const li = document.createElement("li");
+  const div = document.createElement("div");
+  div.setAttribute("class", "store--item-icon");
+  const img = document.createElement("img");
+  img.setAttribute("src", `./assets/icons/${storeItem.id}.svg`);
+  img.setAttribute("alt", storeItem.name);
+
+  const button = document.createElement("button");
+  button.innerText = "Add to cart";
+  button.addEventListener("click", () => {
+    handleItemClick(storeItem);
+  });
+
+  div.appendChild(img);
+
+  li.appendChild(div);
+  li.appendChild(button);
+
+  return li;
+}
+
+renderItems();
+renderCart();
