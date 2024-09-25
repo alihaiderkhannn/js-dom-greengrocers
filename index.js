0195de105b07bf7b9b7e3938d3ad007a50460dac
Ali Haider Khan <ali_786@live.no>
2024-09-25 12:01:04 +0200

    done with core and extension 1

--- index.js
+++ index.js
@@ -3,52 +3,64 @@ const state = {
     {
       id: "001-beetroot",
       name: "beetroot",
-      price: 0.35
+      price: 0.35,
+      type: 'Vegetable'
     },
     {
       id: "002-carrot",
       name: "carrot",
-      price: 0.35
+      price: 0.35,
+      type: 'Vegetable'
     },
     {
       id: "003-apple",
       name: "apple",
-      price: 0.35
+      price: 0.35,
+      type: 'Fruit'
     },
     {
       id: "004-apricot",
       name: "apricot",
-      price: 0.35
+      price: 0.35,
+      type: 'Vegetable'
     },
     {
       id: "005-avocado",
       name: "avocado",
-      price: 0.35
+      price: 0.35,
+      type: 'Vegetable'
     },
     {
       id: "006-bananas",
       name: "bananas",
-      price: 0.35
+      price: 0.35,
+      type: 'Fruit'
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
-      price: 0.35
+      price: 0.35,
+      type: 'Fruit'
+
+      
     },
     {
       id: "008-berry",
       name: "berry",
-      price: 0.35
+      price: 0.35,
+      type: 'Fruit'
     },
     {
       id: "009-blueberry",
       name: "blueberry",
-      price: 0.35
+      price: 0.35,
+      type: 'Fruit'
     },
     {
       id: "010-eggplant",
       name: "eggplant",
-      price: 0.35
+      price: 0.35,
+      type: 'Fruit'
     }
   ],
   cart: []
@@ -86,6 +98,8 @@ function renderItems() {
 }
 function renderCart() {
   const total = document.querySelector(".total-number");
+
+  cartList.innerHTML = "";
   //iterate through state.cart.map  and update the cart view
   state.cart.map((item) => {
     // let li = renderCartItem(item);
@@ -96,22 +110,9 @@ function renderCart() {
   total.innerHTML = calculateTotal();
 }
 function renderCartItem(cartItem) {
-  /*
-  <li>
-  <img
-    class="cart--item-icon"
-    src="assets/icons/001-beetroot.svg"
-    alt="beetroot"
-  />
-  <p>beetroot</p>
-  <button class="quantity-btn remove-btn center">-</button>
-  <span class="quantity-text center">1</span>
-  <button class="quantity-btn add-btn center">+</button>
-</li>
-
-  */
-  const li = document.createElement("li");
-  const img = document.createElement("img");
+  
+  //const li = document.createElement("li");
+  //const img = document.createElement("img");
 
 
   const list = document.querySelector(".cart--item-list");
@@ -134,10 +135,13 @@ function renderCartItem(cartItem) {
   const addbtn = item.querySelector(".add-btn")
   
   removebtn.addEventListener('click', () => {
-    if (x.quantity > 0) {
+    if (x.quantity > 1) {
       x.quantity--;
     } else {
-      state.cart = state.cart.filter(cartItem => cartItem.id !== x.id)
+      console.log(state.cart)
+      x.quantity--;
+      const index = state.cart.findIndex((item) => item.id === x.id);
+      state.cart.splice(index, 1);
     }
     renderCart();
   })
